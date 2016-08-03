05b35f483887a447b7d76c99f765f6ddcb4236b0
an <15501233770@163.com>
2016-08-03 17:27:43 +0800

    i18n

--- src/main/java/com/helian/helloWorld.java
+++ src/main/java/com/helian/helloWorld.java
@@ -1,7 +1,10 @@
 package com.helian;
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;    
+
+import org.junit.Test;
 public class helloWorld extends HttpServlet {
     /**
 	 * 
@@ -27,5 +30,21 @@ public class helloWorld extends HttpServlet {
         out.println("<h1>Hello Dawson.Jack!!!</h1>");
         out.println("</body>");
         out.println("</html>");
+    }
+    @Test
+    public void run(){
+    	String url = "http://www.baidu.com?name=zhang&age=18";
+    	int indexOf = url.indexOf("?");
+    	String substring = url.substring(indexOf+1,url.length());
+    	String substring2 = url.substring(indexOf);
+    	System.out.println(substring);
+    	System.out.println(substring2);
+    	//trim用法
+    	String trim1="aa";
+    	String trim2="a a";
+    	String trim3="  ";
+    	System.out.println("trim1:"+trim1.trim().length());
+    	System.out.println("trim2:"+trim2.trim().length());
+    	System.out.println("trim3:"+trim3.trim().length());
     }
 }
--- src/main/java/com/helian/testVM.java
+++ src/main/java/com/helian/testVM.java
@@ -1,29 +1,26 @@
 package com.helian;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.velocity.context.Context;
-import org.apache.velocity.tools.ToolContext;
-import org.apache.velocity.tools.struts.MessageTool;
-import org.apache.velocity.tools.struts.StrutsUtils;
 import org.apache.velocity.tools.view.VelocityViewServlet;
 import org.apache.velocity.Template;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+
 
 public class testVM extends VelocityViewServlet {
 
@@ -35,28 +32,61 @@ public class testVM extends VelocityViewServlet {
     protected Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context ctx) {
         ctx.put("name", "道森.杰克");
         ctx.put("message","my heart");
-        List<Object> list = new ArrayList<Object>();
-        list.add("yesterday");
-        ctx.put("list", list);
+       // List<Object> list = new ArrayList<Object>();
+        //list.add("yesterday");
+       // ctx.put("list", list);
         System.out.println("道森。杰克");
         
-        //加载spring
-       // ApplicationContext  context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-        
-       // MessageSource messageSource = (MessageSource) context.getBean("messageSource");
-    //    MessageTool messageTool = new MessageTool(new MessageSourceAccessor(messageSource), locale);
-     /*   ctx.put("messageTool", messageTool);
-         
-                        System.out.println("-------begin---------");
-                        System.out.println(messageTool.get("hello。world"));// hello
-                        System.out.println("-------end-----------");*/
         Template template = getTemplate("show.vm");
         return template;
     }
+    
+    public String getName(String name){      
+    	return "hang";
+    }
+    
     @Test
-    public void run(){      
-    	String line =" ";
-    	String string = line.split("=")[0];
+    public void test(){
+    	String string = get("hello.world");
     	System.out.println(string);
     }
-}
+    public String get(String key) {
+        try {
+        	//ApplicationContext  context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+           // MessageSource messageSource = (MessageSource) context.getBean("messageSource");
+           // if (messageSource != null) {
+            	//获取所在地区语言
+            	Locale locale2 = Locale.getDefault();  
+
+            	Locale locale3 = new Locale("zh", "CN"); 
+            	ResourceBundle messages =ResourceBundle.getBundle("i18n/abc", locale3);
+            	String message = messages.getString(key);
+            	System.out.println(message);
+             //  String message = messageSource.getMessage(key, args, key, locale2);
+               
+               return message;
+          //  }
+          //  return key;
+        } catch (NoSuchMessageException e) {
+            return key;
+        }
+    }
+
+    }
+    
+//加载spring
+    
+    
+    /*public String get(String key) {
+        try {
+        	ApplicationContext  context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+            MessageSource messageSource = (MessageSource) context.getBean("messageSource");
+            if (messageSource != null) {
+            	Locale locale = null;
+                return key;
+            }
+            return key;
+        } catch (NoSuchMessageException e) {
+            return key;
+        }
+    }*/
